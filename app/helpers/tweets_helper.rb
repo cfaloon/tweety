9e10bbb83148292ca360e7c01019f29b37da0b48
cfaloon <cfaloon@gmail.com>
2018-07-23 10:02:06 -0400

    Day 2 commit

---
+++ app/helpers/tweets_helper.rb
@@ -0,0 +1,16 @@
+module TweetsHelper
+  def get_tagged(tweet)
+    message_arr = tweet.message.split
+
+    message_arr.each_with_index do |word, index|
+      if word[0] == '#'
+        tag = Tag.find_or_create_by(phrase: word.downcase)
+        TweetTag.create(tag_id: tag.id, tweet_id: tweet.id)
+        message_arr[index] = "<a href='/tag_tweets?id=#{tag.id}'>#{word}</a>"
+      end
+    end
+
+    tweet.message = message_arr.join(' ')
+    tweet.save
+  end
+end
